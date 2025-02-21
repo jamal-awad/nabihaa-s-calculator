fbd2f481453d7661d24050e065e1a34976599631
jamal-awad <jamalawad428@gmail.com>
2025-02-21 21:27:12 +0200

    Initial commit with nabiha's calculator script

---
+++ nabiha's calculator/nabiha's program.py
@@ -0,0 +1,5 @@
+salary = int(input("Enter your salary: "))
+monthes = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+month = input("enter the month of salary: ")
+if month not in monthes:
+        print ("write the month in letters")
